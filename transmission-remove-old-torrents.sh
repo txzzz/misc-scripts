a698c4e8ddbda0c7dcad1b70354b5229234aab27
txzzz <toxaz0@gmail.com>
2022-10-08 17:38:25 +0200

    first commit

---
+++ transmission-remove-old-torrents.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+transmission="/usr/bin/transmission-remote"
+user=""
+pass=""
+
+OLDIFS=$IFS
+IFS="
+"
+for torrent in $($transmission -n $user:$pass -l | grep 100%); do
+	id=$(echo $torrent | sed "s/^ *//g" | sed "s/ *100%.*//g")
+	finished=$(date -d $($transmission -n $user:$pass -t $id -i | grep 'Date finished' | sed -n -e 's/^.*Date finished: *//p') +%s)
+	now=$(date +%s)
+	if [ $(( $now - $finished )) -gt 259200 ]; then
+		$transmission -n $user:$pass -t $id --remove-and-delete
+	fi
+done
+IFS=$OLDIFS
