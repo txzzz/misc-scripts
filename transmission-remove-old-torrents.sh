fa7b3a31d1c42727588d324feee268821ef8ce65
txzzz <toxaz0@gmail.com>
2022-10-23 09:28:47 +0200

    missed a 0, torrents were removed after 3 days instead of 30

--- transmission-remove-old-torrents.sh
+++ transmission-remove-old-torrents.sh
@@ -11,7 +11,7 @@ for torrent in $($transmission -n $user:$pass -l | grep 100%); do
 	id=$(echo $torrent | sed "s/^ *//g" | sed "s/ *100%.*//g")
 	finished=$(date -d $($transmission -n $user:$pass -t $id -i | grep 'Date finished' | sed -n -e 's/^.*Date finished: *//p') +%s)
 	now=$(date +%s)
-	if [ $(( $now - $finished )) -gt 259200 ]; then
+	if [ $(( $now - $finished )) -gt 2592000 ]; then
 		$transmission -n $user:$pass -t $id --remove-and-delete
 	fi
 done
