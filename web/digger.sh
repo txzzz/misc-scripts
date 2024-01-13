98d8d315aab381b21e8dc6b43499fb9c357a8829
txzzz <toxaz0@gmail.com>
2024-01-13 09:40:55 +0100

    Adding digger script

---
+++ web/digger.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $1 == '-h' || $1 == '--help' ]]; then
+	echo "Usage: ./digger.sh <comma or space separated list of hostnames>"
+	echo "Does a DNS lookup on each hostname in the list and displays reverse DNS if the IP has it."
+	exit
+fi
+
+domains=$(echo $@ | tr ',' ' ' | xargs)
+
+for domain in $domains; do
+        echo "$ dig +short ${domain}"
+        ips="$(dig +short $domain | tr '\n' ' ' | tr -d '\r')"
+        for ip in $ips; do
+                if [[ $ip == "" ]]; then
+                        echo "No A record!"
+                        continue
+                fi
+                if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+                        reverse=$(dig +short -x $ip)
+                        echo "${ip} ${reverse}"
+                fi
+        done
+done
