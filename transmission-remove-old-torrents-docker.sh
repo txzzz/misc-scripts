23a9c1fbe74fd7a9c4f75ad4250e98a23765cf62
txzzz <toxaz0@gmail.com>
2024-01-20 09:53:35 +0100

    Docker support for torrent remover

---
+++ transmission-remove-old-torrents-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+user=""
+pass=""
+container="transmission"
+days=30
+transmission="/usr/bin/transmission-remote"
+
+torrents="$(docker exec -it $container $transmission -n $user:$pass -l | grep '100%')"
+
+OLDIFS=$IFS
+IFS="
+"
+for torrent in $torrents; do
+	id=$(echo $torrent | sed "s/^ *//g" | sed "s/ *100%.*//g")
+	finished=$(date -d $(docker exec -it $container $transmission -n $user:$pass -t $id -i | grep 'Date finished' | sed -n -e 's/^.*Date finished: *//p') +%s)
+	if (( ($(date +%s) - $finished) > ($days * 86400) )); then
+		docker exec -it $container $transmission -n $user:$pass -t $id --remove-and-delete
+	fi
+done
+IFS=$OLDIFS
