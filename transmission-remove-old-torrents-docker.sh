74a3be69fff85a852a83061bb5b9b1b43e813a6f
txzzz <toxaz0@gmail.com>
2024-01-21 09:38:01 +0100

    changed docker exec -it to -t to work in cron

--- transmission-remove-old-torrents-docker.sh
+++ transmission-remove-old-torrents-docker.sh
@@ -6,16 +6,16 @@ container="transmission"
 days=30
 transmission="/usr/bin/transmission-remote"
 
-torrents="$(docker exec -it $container $transmission -n $user:$pass -l | grep '100%')"
+torrents="$(docker exec -t $container $transmission -n $user:$pass -l | grep '100%')"
 
 OLDIFS=$IFS
 IFS="
 "
 for torrent in $torrents; do
 	id=$(echo $torrent | sed "s/^ *//g" | sed "s/ *100%.*//g")
-	finished=$(date -d $(docker exec -it $container $transmission -n $user:$pass -t $id -i | grep 'Date finished' | sed -n -e 's/^.*Date finished: *//p') +%s)
+	finished=$(date -d $(docker exec -t $container $transmission -n $user:$pass -t $id -i | grep 'Date finished' | sed -n -e 's/^.*Date finished: *//p') +%s)
 	if (( ($(date +%s) - $finished) > ($days * 86400) )); then
-		docker exec -it $container $transmission -n $user:$pass -t $id --remove-and-delete
+		docker exec -t $container $transmission -n $user:$pass -t $id --remove-and-delete
 	fi
 done
 IFS=$OLDIFS
